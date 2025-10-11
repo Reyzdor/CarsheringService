e09248c8cac6e7ad1f5dc5dc5667b4a8b3d1ec35
Roman <m3f3dr0nbristleback@gmail.com>
2025-10-11 18:48:22 +0300

    initial commit

---
+++ CarsheringService/CarsheringServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsheringServiceApp.swift
+//  CarsheringService
+//
+//  Created by Roman on 11.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarsheringServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
