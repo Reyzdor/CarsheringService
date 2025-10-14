4c1ff1630570077b78ca69e8400a14f8547d94f0
Roman <m3f3dr0nbristleback@gmail.com>
2025-10-14 21:04:12 +0300

    update +

---
+++ CarsheringService/Client/Components/SmallCard.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct SmallCardView: View {
+    let iconName: String
+    let title: String
+    let description: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 16)
+            .fill(Color(red: 0.90, green: 0.30, blue: 1.0))
+            .frame(width: 100, height: 200)
+            .overlay(
+                VStack {
+                    Image(iconName)
+                        .renderingMode(.template)
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .offset(y: -30)
+                        .foregroundColor(Color(red: 0.0, green: 1.0, blue: 0.0))
+                        .padding(10)
+                    
+                    Text(title)
+                        .font(.caption)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .offset(y: -20)
+                    
+                    Text(description)
+                        .font(.system(size: 10))
+                        .foregroundColor(.white.opacity(0.7))
+                        .multilineTextAlignment(.center)
+                        .lineLimit(2)
+                }
+            )
+    }
+}
